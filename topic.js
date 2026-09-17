79e228641721d5985cd9bb0bb5bca24934b5f7bf
sadi-25 <shoriot.sadi@gmail.com>
2026-09-17 19:51:45 +0600

    Update topic.js

--- topic.js
+++ topic.js
@@ -214,7 +214,7 @@ function renderContent(type = "all") {
                 ${item.description}
             </p>
     ${item.author ? `<div class="book-author"><strong>লেখক:</strong> ${item.author}</div>` : ""}
-    ${item.source ? `<div class="book-source"><strong>Source:</strong> ${item.source}</div>` : ""}
+    ${item.source ? `<div class="book-source"><strong>Source:</strong> <a href="${item.source}" target="_blank">IslamHouse</a></div>` : ""}
             <a href="${item.link || item.file}">
                 ${item.linkText || "বইটি পড়ুন →"}
             </a>
